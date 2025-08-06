a3c1fe10fc29366adb06f53c983b727b9a543459
yanhua.guo <gyhmichael@163.com>
2025-08-06 17:00:51 +0800

    会话记忆

--- src/main/java/com/example/demo/service/AIDoctorServiceFactory.java
+++ src/main/java/com/example/demo/service/AIDoctorServiceFactory.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.service.AiServices;
 import jakarta.annotation.Resource;
@@ -22,7 +24,12 @@ public class AIDoctorServiceFactory {
     //配置bean
     @Bean
     public AIDoctorService aiDoctorService() {
-        return AiServices.create(AIDoctorService.class, qwenChatModel);
+        ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(10);
+
+        return AiServices.builder(AIDoctorService.class)
+                .chatModel(qwenChatModel)
+                .chatMemory(chatMemory)//会话记忆
+                .build();
     }
 
 }
--- src/test/java/com/example/demo/service/AIDoctorServiceTest.java
+++ src/test/java/com/example/demo/service/AIDoctorServiceTest.java
@@ -16,4 +16,12 @@ class AIDoctorServiceTest {
         String chat = aiDoctorService.chat("你好，我是C#程序员andy，最近腿部不舒服，不知道是什么情况");
         System.out.println(chat);
     }
+
+    @Test
+    void chatWithMemory() {
+        String chat = aiDoctorService.chat("你好，我是C#程序员andy，最近腰部不舒服，不知道是什么情况");
+        System.out.println(chat);
+        chat = aiDoctorService.chat("这和我的职业有多大关系？");
+        System.out.println(chat);
+    }
 }
